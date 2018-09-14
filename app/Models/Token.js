3a79c0cf6cf82bf26f30ec4483abd361556e676d
Romain Lanz <romain.lanz@slynova.ch>
2018-09-15 00:54:22 +0200

    chore: initial commit

---
+++ app/Models/Token.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/** @typedef {typeof import('@adonisjs/lucid/src/Lucid/Model')} Model */
+
+/** @type {Model} */
+const Model = use('Model')
+
+class Token extends Model {
+
+}
+
+module.exports = Token
